87086afd519b3f03bf434353ab26bd8697e2d589
Ariel <99064413+arielwhittingham@users.noreply.github.com>
2023-12-22 18:35:00 -0500

    boilerpalte

---
+++ clip.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	echo $PATH
+fi
---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Generate AppleScript code
+applescript_code="display dialog \"Hello, world!\""
+
+# Write code to a file
+echo "$applescript_code" > my_applescript.scpt
